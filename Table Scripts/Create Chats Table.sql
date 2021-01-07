e9677a4f7ff9f76aaef017cbc718294d402b0d9b
Anakaren <ak.rojas.07@gmail.com>
2021-01-07 11:26:34 -0800

    updated table to include matchid instead of chatid

--- Table Scripts/Create Chats Table.sql
+++ Table Scripts/Create Chats Table.sql
@@ -1,7 +1,7 @@
 Create table dbo.Chats 
 (
 	Id bigint not null primary key identity(1,1),
-	ChatId bigint not null,
+	MatchId bigint not null foreign key references dbo.Matches(Id),
 	FirstUserId bigint not null foreign key references dbo.Users(Id),
 	SecondUserId bigint not null foreign key references dbo.Users(Id),
 	[Message] varchar(max),
--- Table Scripts/Insert Mock data Chats.sql
+++ Table Scripts/Insert Mock data Chats.sql
@@ -1,7 +1,5 @@
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (1, 35, 34, 'Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', '3/16/2020');
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (3, 42, 31, 'Nulla nisl.', '7/18/2020');
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (2, 31, 35, 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', '2/23/2020');
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (3, 31, 42, 'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.', '8/26/2020');
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (4, 33, 35, 'Donec vitae nisi.', '11/5/2020');
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (5, 35, 42, 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla.', '11/10/2019');
-insert into dbo.Chats (ChatId, FirstUserId, SecondUserId, Message, DateSent) values (5, 42, 35, 'Ut tellus.', '11/30/2019');
+insert into dbo.Chats (MatchId, FirstUserId, SecondUserId, Message, DateSent) values (60, 42, 31, 'Nulla nisl.', '7/18/2020');
+insert into dbo.Chats (MatchId, FirstUserId, SecondUserId, Message, DateSent) values (54, 31, 29, 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', '2/23/2020');
+insert into dbo.Chats (MatchId, FirstUserId, SecondUserId, Message, DateSent) values (73, 33, 42, 'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.', '8/26/2020');
+insert into dbo.Chats (MatchId, FirstUserId, SecondUserId, Message, DateSent) values (70, 35, 42, 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla.', '11/10/2019');
+insert into dbo.Chats (MatchId, FirstUserId, SecondUserId, Message, DateSent) values (70, 42, 35, 'Ut tellus.', '11/30/2019');
